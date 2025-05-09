55a75cd3f3b949978c8169c94260ee33cea3cb13
Vorps Technologies <161806084+gamedevteam321@users.noreply.github.com>
2025-05-09 16:59:42 +0530

    Scroll behaviour
    Scroll behaviour

--- src/components/sections/Impact.tsx
+++ src/components/sections/Impact.tsx
@@ -46,7 +46,7 @@ function ImpactCards() {
 export default function Impact() {
   return (
     <section id="impact" className="bg-gradient-to-b from-[#070707] via-[#18181b] to-[#232325] pb-4  md:py-20">
-      <div className="container mx-auto px-6 md:px-12 lg:px-16 space-y-3 flex flex-col items-center">
+      <div className="container px-6 md:px-12 lg:px-16 space-y-3 flex flex-col items-center">
         <h2 className="text-4xl md:text-7xl font-bold text-white text-center">
           Impacting <span className="bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-500 bg-clip-text text-transparent">Bharat</span>
         </h2>
@@ -55,14 +55,14 @@ export default function Impact() {
           <p className="text-white text-2xl md:text-[35px] font-regular text-center mt-2">Empower, Educate, Enrich</p>
         </div>
         <ImpactCards />
-        <div id="media">
+        <div id="media" className="w-full">
           <h2 className="text-4xl md:text-5xl font-bold text-white text-center pt-10">
             In the <span className="bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-500 bg-clip-text text-transparent">Media</span>
           </h2>
-          <div className="flex flex-col items-center ">
+          <div className="flex flex-col w-full items-center ">
             <p className="text-gray-400 text-xl md:text-3xl font-regular text-center leading-[1.75rem] tracking-[-0.8px]">Media Snapshots of<span className="text-white"> Saathi's Journey</span></p>
+            <VideoTickerComponent videos={impactVideoData} title="" />
           </div>
-          <VideoTickerComponent videos={impactVideoData} title="" />
         </div>
       </div>
     </section>
